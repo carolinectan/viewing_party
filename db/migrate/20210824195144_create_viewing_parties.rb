2d7a19689ae1cf03616921139e23d159230341f8
Matt Kragen <mattdkragen@gmail.com>
2021-08-24 12:58:23 -0700

    Reconfigure schema; generate models

--- db/migrate/20210824195144_create_viewing_parties.rb
+++ db/migrate/20210824195144_create_viewing_parties.rb
@@ -2,10 +2,9 @@ class CreateViewingParties < ActiveRecord::Migration[5.2]
   def change
     create_table :viewing_parties do |t|
       t.integer :duration
-      t.date :date
       t.time :start_time
+      t.date :date
       t.integer :movie_id
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210824195317_create_attendees.rb
+++ db/migrate/20210824195317_create_attendees.rb
@@ -1,8 +1,9 @@
 class CreateAttendees < ActiveRecord::Migration[5.2]
   def change
     create_table :attendees do |t|
-      t.references :user, foreign_key: true
       t.references :viewing_party, foreign_key: true
+      t.references :user, foreign_key: true
+      t.boolean :host
 
       t.timestamps
     end
