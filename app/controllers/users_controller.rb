38fb92910c9d75ca0a22ae4376df733afef50130
Caroline Tan <carolinectan3@gmail.com>
2021-08-24 19:44:53 -0400

    Add login form; no params passing through

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def login_form
+  end
+
+  def login
+# require "pry"; binding.pry
+  end
+
+  # private
+  # def user_params
+  #   params.require(:user).permit(:email, :password_digest)
+  # end
+end
