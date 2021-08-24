1f31a6e012f188864cd6eb4567944db67be80a4a
Caroline Tan <carolinectan3@gmail.com>
2021-08-24 19:44:27 -0400

    Add login tests

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'logging in' do
+  before :each do
+    @user = User.create(email: 'ilovedogs@gmail.com', password_digest: 'test')
+
+    visit root_path
+  end
+
+  it 'can log in with valid credentials' do
+    click_button 'Log In'
+
+    expect(current_path).to eq(login_path)
+save_and_open_page
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+
+    click_button 'Log In'
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to have_content("Welcome, #{@user.email}!")
+  end
+end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,27 +5,28 @@ RSpec.describe 'welcome page' do
     visit "/"
   end
 
-  it 'has a welcome message' do
-    expect(page).to have_content('Welcome to Viewing Party!')
+  describe 'welcome page message and description' do
+    it 'has a welcome message' do
+      expect(page).to have_content('Welcome to Viewing Party!')
+    end
+
+    it 'has a welcome description' do
+      expect(page).to have_content('Where you view movies with your friends')
+    end
   end
 
-  it 'has a welcome description' do
-    expect(page).to have_content('Where you view movies with your friends')
+  describe 'login' do
+    it 'has a button to login' do
+      expect(page).to have_button('Log In')
+    end
   end
 
-  it 'has a button to login' do
-    expect(page).to have_button('Log In')
-  end
-
-  it 'has a link to registration' do
-    expect(page).to have_link('Register')
+  describe 'registration' do
+    it 'has a link to registration' do
+      expect(page).to have_link('Register')
+    end
   end
 end
 
-# When a user visits the root path they should be on the welcome page which includes:
-#
-#  Welcome message
-#  Brief description of the application
-#  Button to Log in
-#  Link to Registration
+
 # Details: Implement basic auth in the application allowing a user to log in with an email and password. The password should be stored in the database using bcrypt.
