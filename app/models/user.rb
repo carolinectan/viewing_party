190a22eafcdf5fef33bbd2c39f79922845ceb1f4
Caroline Tan <carolinectan3@gmail.com>
2021-08-24 21:51:33 -0400

    Refactor with has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :parties, through: :attendees
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password_digest, require: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
 end
