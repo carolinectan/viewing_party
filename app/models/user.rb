4feafc477515c1b8b194329ddbb4b34fc14a064f
Caroline Tan <carolinectan3@gmail.com>
2021-08-24 21:49:03 -0400

    Add model validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :attendees
   has_many :parties, through: :attendees
+
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password_digest, require: true
 end
