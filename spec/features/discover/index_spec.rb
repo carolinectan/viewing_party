40a9f2d4acbd2ac0e0905ae037abdcc7db0b7baa
Matt Kragen <mattdkragen@gmail.com>
2021-08-26 11:07:19 -0700

    Test discover page for button_to movies_path

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Discover index page" do
+  describe 'features' do
+    it 'contains button to discover top 40 movies' do
+      visit discover_path
+
+      expect(page).to have_button("Discover Top 40 Movies")
+    end
+
+    it 'discover button visits movies_path' do
+      visit discover_path
+
+      click_button "Discover Top 40 Movies"
+
+      expect(current_path).to eq(movies_path)
+    end
+  end
+end
