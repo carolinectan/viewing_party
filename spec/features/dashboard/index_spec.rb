58f44bd51e6bcbdcf322aded2998d4d21f0dbcec
Matt Kragen <mattdkragen@gmail.com>
2021-08-25 18:44:18 -0700

    Write out dashboard tests

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -13,19 +13,44 @@ RSpec.describe "Dashboard page" do
   describe 'features' do
     it 'displays greeting to authenticated user' do
       visit login_path
-
+  
       user = User.create(email: 'ilovedogs@gmail.com', password: 'test')
       
       fill_in :email, with: user.email
       fill_in :password, with: user.password
-
+  
       click_on 'Log In'
-
       expect(page).to have_content("Welcome, #{user.email}!")
     end
 
-    xit 'displays friends of user' do
-  
+    context "Friends" do
+      xit 'displays friends of user' do
+        visit dashboard_path
+        
+        within("#friends") do
+          expect(page).to have_field(friend_search)
+        end
+      end
+    end
+
+    context "Viewing Parties" do
+      xit 'displays viewing parties this user has created' do
+        within("#my-parties") do
+          # link_to movie
+          # date/time
+          # You are the host
+          # list of invited friends
+        end
+      end
+      
+      xit 'displays viewing parties this user has been invited to' do
+        within("#party-invites") do
+          # link_to movie
+          # date/time
+          # You are the host
+          # list of invited friends
+        end
+      end
     end
   end
 end
