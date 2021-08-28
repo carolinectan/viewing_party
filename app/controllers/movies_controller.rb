ba8721fe32ea14c1ebffd552ad9ab4ef75b02933
Matt Kragen <mattdkragen@gmail.com>
2021-08-27 17:18:49 -0700

    Fill out #top_movies for MovieService

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = MovieService.new
+    @movies = MovieService.new.top_movies
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -5,16 +5,11 @@ class MovieService < ApiService
     data = get_data('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1').get do |req|
       req.params['api_key'] = ENV['movie_api_key']
     end
+    
+    parsed_data = parsed(data)
 
-    @movies = []
-
-    data[:results].each do |movie|
-      @movies << Movie.new(movie[:title], movie[:vote_average])
+    parsed_data[:results].map do |movie|
+      Movie.new(movie[:title], movie[:vote_average])
     end
   end
-
-  # should movies live here
-  # controller / how can we access @movies array
-  # MovieService.new.movies
-  # how to utilize the poro
 end
