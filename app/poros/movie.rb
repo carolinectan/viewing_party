52366c53a2e7295be2e91fd06496a5a8b91f8efa
Matt Kragen <mattdkragen@gmail.com>
2021-08-30 14:57:25 -0700

    Refactor + include VCR in old test

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,17 +1,9 @@
 class Movie
   attr_reader :title, :vote_average, :id
 
-  def initialize(title, vote_average, id)
-    @title = title
-    @vote_average = vote_average
-    @id = id
+  def initialize(hash)
+    @title = hash[:title]
+    @vote_average = hash[:vote_average]
+    @id = hash[:id]
   end
-
-  # def initialize(hash)
-  #   @title = hash[:title]
-  #   @vote_average = hash[:vote_average]
-  #   @id = hash[:id]
-  # end
-
-  #will create movie object with values or with nil
 end
