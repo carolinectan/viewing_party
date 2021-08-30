034eb6995884f942d31950776475359093046355
Matt Kragen <mattdkragen@gmail.com>
2021-08-30 15:35:00 -0700

    Configure presentation logic

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,20 @@
 class Movie
-  attr_reader :title, :vote_average, :id
+  attr_reader :title,
+              :vote_average,
+              :id,
+              :runtime,
+              :genres,
+              :summary
 
   def initialize(hash)
     @title = hash[:title]
     @vote_average = hash[:vote_average]
     @id = hash[:id]
+    @runtime = hash[:runtime]
+    @genres = hash[:genres].map do |genre|
+      genre[:name]
+    end
+    require 'pry'; binding.pry
+    @summary = hash[:overview]
   end
 end
