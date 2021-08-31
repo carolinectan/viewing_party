62afc00b0162b52f310ed72b4b554bd14a6717cf
Caroline Tan <carolinectan3@gmail.com>
2021-08-30 23:32:47 -0400

    Refactor genres; details api method

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -11,13 +11,7 @@ class Movie
     @vote_average = hash[:vote_average]
     @id = hash[:id]
     @runtime = hash[:runtime]
-    @genres = get_genres(hash)
+    @genres = hash[:genres]
     @summary = hash[:overview]
-  end
-
-  def get_genres(hash)
-    hash[:genres].map do |genre|
-      genre[:name]
-    end
   end
 end
