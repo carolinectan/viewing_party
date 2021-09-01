5d0f2d6e3033f3a2f4627ac63a5c0c3023f4580a
Caroline Tan <carolinectan3@gmail.com>
2021-08-31 21:27:50 -0400

    Add sessions controller, current_user functionality

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -30,11 +30,49 @@ RSpec.describe "Dashboard page" do
       it 'displays friends of user' do
         visit dashboard_path
 
-        within("#friends") do
+        within "#friends" do
           expect(page).to have_content("Friends")
         end
       end
+
+      xit "displays a text field to enter a friend's email and a button to add friend" do
+        # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        # session user id - application controller
+
+        visit dashboard_path
+
+        within "#friends" do
+          expect(page).to have_content('You currently have no friends')
+
+          fill_in "Friend's Email:", with: 'dogsrule@email.com'
+          click_on "Add Friend"
+        end
+
+        expect(current_path).to eq(discover_path)
+
+        within "#friends" do
+          expect(page).to have_content('dogsrule@email.com')
+        end
+      end
     end
+# As an authenticated user,
+# Scenarios:
+#
+# If I have not added any friends there should be a message. "You currently have no friends".
+#
+# If I have added friends, I should see a list of all my friends.
+#
+# Details: Users should be able to add a friend by their email address, as long as, the friend is a user of our application and exists in our database.
+#
+# Example:
+# Bugs Bunny and Lola Bunny are users of our application, but Daffy Duck is not.
+#
+# When Bugs Bunny enters lola_bunny@gmail.com to add friend it will be successful and Lola should show up as Bugs Bunny's friend.
+# When Bugs Bunny enters daffy_duck@gmail.com to add friend it should give an error message that the user does not exist.
+# Tips: You'll want to research self-referential has_many through. Here is a good starting point to understand the concept. You will probably need to do more googling but that's part of the fun ;)
+#
+# Write a happy path test
+# Write a sad path test
 
     context "Viewing Parties" do
       before :each do
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'logging in' do
-  before :each do
-    @user = User.create(email: 'ilovedogs@gmail.com', password: 'test')
-  end
-
   it 'can log in with valid credentials' do
+    @user = User.create(email: 'ilovedogs@gmail.com', password: 'test')
+
     visit root_path
 
     click_on 'Log In'
@@ -18,10 +16,23 @@ RSpec.describe 'logging in' do
     click_on 'Log In'
 
     expect(current_path).to eq(dashboard_path)
+
     expect(page).to have_content("Welcome, #{@user.email}!")
+    expect(page).to_not have_link('Log In')
+    expect(page).to_not have_link('Register')
+    expect(page).to have_link('Log Out')
+
+    click_link 'Log Out'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_link('Log In')
+    expect(page).to have_link('Register')
+    expect(page).to_not have_link('Log Out')
   end
 
   it "cannot log in with bad credentials" do
+    @user = User.create(email: 'ilovedogs@gmail.com', password: 'test')
+
     visit login_path
 
     fill_in :email, with: @user.email
@@ -31,6 +42,6 @@ RSpec.describe 'logging in' do
 
     expect(current_path).to eq(login_path)
 
-    expect(page).to have_content("Sorry, your credentials are bad.")
+    expect(page).to have_content("Sorry, your credentials were bad.")
   end
 end
