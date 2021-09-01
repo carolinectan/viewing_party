7df1732135b52c39a3ef3dd231b75819c951c96f
Caroline Tan <carolinectan3@gmail.com>
2021-09-01 02:32:36 -0400

    Add authentication to discover test and index view

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe "Discover index page" do
   describe 'features' do
     it 'contains button to discover top 40 movies' do
+      user = User.create(email: 'ilovedogs@gmail.com', password: 'test1')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit discover_path
 
       expect(page).to have_button('Discover Top 40 Movies')
     end
 
     it 'discover button visits movies_path' do
+      user = User.create(email: 'ilovedogs@gmail.com', password: 'test1')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       VCR.use_cassette('movie_details') do
         visit discover_path
 
