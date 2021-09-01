e6d61cd781824770a16a34e05a9608da42c1605c
Caroline Tan <carolinectan3@gmail.com>
2021-09-01 01:37:48 -0400

    Remove comments

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,4 @@
 class FriendshipsController < ApplicationController
-  # def index
-  #
-  # end
-  # def show
-  #
-  # end
-  # def new
-  # end
   def create
     user = current_user
     friend = User.find_by(email: params[:email])
@@ -15,16 +7,8 @@ class FriendshipsController < ApplicationController
 
     redirect_to dashboard_path
   end
-  # def edit
-  #
-  # end
-  # def update
-  #
-  # end
-  # def destroy
-  #
-  # end
   # private
+
   # def _params
   #   params.permit(:)
   # end
