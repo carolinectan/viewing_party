6197bca03d39090b5e4410e5148cca1b935c2340
Caroline Tan <carolinectan3@gmail.com>
2021-09-01 01:20:43 -0400

    Add unauth user partial/redirect; add stub to dashboard tests for auth user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
-    @friends = current_user.friends
+    if current_user
+      @friends = current_user.friends
+    end
   end
 end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Dashboard page' do
   describe 'navigation' do
     it 'links to Discover page' do
+      user = User.create(email: 'ilovedogs@gmail.com', password: 'test1')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit dashboard_path
 
       click_on 'Discover Movies'
@@ -27,7 +30,10 @@ RSpec.describe 'Dashboard page' do
     end
 
     describe 'Friends' do
-      it 'displays friends of user' do
+      it 'displays a friends section' do
+        user = User.create(email: 'ilovedogs@gmail.com', password: 'test1')
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
         visit dashboard_path
 
         within '#friends' do
@@ -59,28 +65,43 @@ RSpec.describe 'Dashboard page' do
           expect(page).to have_content('dogsrule@gmail.com')
         end
       end
+
+      it 'redirects unauthorized user to login page' do
+        visit dashboard_path
+
+        expect(current_path).to eq(dashboard_path)
+        expect(page).to have_content('Sorry, you are not allowed to access this page. Please click the link below to log in.')
+        expect(page).to have_link('Log In')
+      end
     end
-# As an authenticated user,
-# Scenarios:
-#
-# If I have not added any friends there should be a message. "You currently have no friends".
-#
-# If I have added friends, I should see a list of all my friends.
-#
-# Details: Users should be able to add a friend by their email address, as long as, the friend is a user of our application and exists in our database.
-#
-# Example:
-# Bugs Bunny and Lola Bunny are users of our application, but Daffy Duck is not.
-#
-# When Bugs Bunny enters lola_bunny@gmail.com to add friend it will be successful and Lola should show up as Bugs Bunny's friend.
-# When Bugs Bunny enters daffy_duck@gmail.com to add friend it should give an error message that the user does not exist.
-# Tips: You'll want to research self-referential has_many through. Here is a good starting point to understand the concept. You will probably need to do more googling but that's part of the fun ;)
-#
-# Write a happy path test
-# Write a sad path test
+
+    # As an authenticated user,
+    # I see a section for friends,
+    # In this section, there should be a text field to enter a friend's email and a button to "Add Friend"
+    #
+    # Scenarios:
+    #
+    # If I have not added any friends there should be a message. "You currently have no friends".
+    #
+    # If I have added friends, I should see a list of all my friends.
+    #
+    # Details: Users should be able to add a friend by their email address, as long as, the friend is a user of our application and exists in our database.
+    #
+    # Example:
+    # Bugs Bunny and Lola Bunny are users of our application, but Daffy Duck is not.
+    #
+    # When Bugs Bunny enters lola_bunny@gmail.com to add friend it will be successful and Lola should show up as Bugs Bunny's friend.
+    # When Bugs Bunny enters daffy_duck@gmail.com to add friend it should give an error message that the user does not exist.
+    # Tips: You'll want to research self-referential has_many through. Here is a good starting point to understand the concept. You will probably need to do more googling but that's part of the fun ;)
+    #
+    #  Write a happy path test
+    #  Write a sad path test
 
     context "Viewing Parties" do
       before :each do
+        user = User.create(email: 'ilovedogs@gmail.com', password: 'test1')
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
         visit dashboard_path
       end
 
